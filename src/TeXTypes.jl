0b00efa9d1d78c939b9452c36dfcce0bdaf48a2e
Robert Moss <rmoss92@gmail.com>
2020-08-12 18:15:01 -0700

    Tabs to spaces (4)

--- src/TeXTypes.jl
+++ src/TeXTypes.jl
@@ -1,74 +1,74 @@
 getrandtexname() = string("tex_", lowercase(basename(tempname())))
 
 mutable struct TeXPackage
-	name::String
-	option::String
+    name::String
+    option::String
 
-	TeXPackage(option, name) = new(name, option)
-	TeXPackage(name) = new(name, "")
+    TeXPackage(option, name) = new(name, option)
+    TeXPackage(name) = new(name, "")
 end
 
 mutable struct TeXCommand
-	name::String
-	value::String
+    name::String
+    value::String
 
-	TeXCommand(name, value) = new(name, value)
-	TeXCommand(name) = new(name, "")
+    TeXCommand(name, value) = new(name, value)
+    TeXCommand(name) = new(name, "")
 end
 
 mutable struct TeXSection
-	name::String
-	body::String
-	needs_section_name::Bool
+    name::String
+    body::String
+    needs_section_name::Bool
 
-	TeXSection(name = "", body = "", needs_section_name = true) = new(isempty(name) ? getrandtexname() : name, body, isempty(name) ? false : needs_section_name)
+    TeXSection(name = "", body = "", needs_section_name = true) = new(isempty(name) ? getrandtexname() : name, body, isempty(name) ? false : needs_section_name)
 end
 
 mutable struct TeXDocument
-	jobname::String
-	documentclass::String
-	documentfontsizept::Integer
-	packages::Array{TeXPackage}
-	preamble::String
-	commands::Array{TeXCommand}
-	inputs::Array{TeXSection}
-	build_dir::String
-	title::String
-	open::Bool
+    jobname::String
+    documentclass::String
+    documentfontsizept::Integer
+    packages::Array{TeXPackage}
+    preamble::String
+    commands::Array{TeXCommand}
+    inputs::Array{TeXSection}
+    build_dir::String
+    title::String
+    open::Bool
 
-	TeXDocument(jobname, documentclass, documentfontsizept, packages, preamble, commands, inputs, build_dir, title, open) = new(jobname, documentclass, documentfontsizept, packages, preamble, commands, inputs, build_dir, title, open)
+    TeXDocument(jobname, documentclass, documentfontsizept, packages, preamble, commands, inputs, build_dir, title, open) = new(jobname, documentclass, documentfontsizept, packages, preamble, commands, inputs, build_dir, title, open)
 
-	function TeXDocument()
-		jobname = "main"
-		documentclass = "article"
-		documentfontsizept = 11
-		packages = TeXPackage[
-			TeXPackage("english", "babel"),
-			TeXPackage("usenames, dvipsnames", "xcolor"),
-			TeXPackage("pdfpages"),
-			TeXPackage("amsmath"),
-			TeXPackage("listings"),
-			TeXPackage("tikz"),
-			TeXPackage("beramono"),
-			TeXPackage("fontenc"),
-			TeXPackage("inconsolata"),
-			# TeXPackage("T1", "fontenc"), % blurs pdf2svg
-		]
-		preamble = read(joinpath(dirname(pathof(TeX)), "..", "include", "julia_preamble.tex"), String)
-		inputs = TeXSection[]
-		commands = TeXCommand[]
-		build_dir = joinpath(".", "")
-		title = ""
-		open = true
+    function TeXDocument()
+        jobname = "main"
+        documentclass = "article"
+        documentfontsizept = 11
+        packages = TeXPackage[
+            TeXPackage("english", "babel"),
+            TeXPackage("usenames, dvipsnames", "xcolor"),
+            TeXPackage("pdfpages"),
+            TeXPackage("amsmath"),
+            TeXPackage("listings"),
+            TeXPackage("tikz"),
+            TeXPackage("beramono"),
+            TeXPackage("fontenc"),
+            TeXPackage("inconsolata"),
+            # TeXPackage("T1", "fontenc"), % blurs pdf2svg
+        ]
+        preamble = read(joinpath(dirname(pathof(TeX)), "..", "include", "julia_preamble.tex"), String)
+        inputs = TeXSection[]
+        commands = TeXCommand[]
+        build_dir = joinpath(".", "")
+        title = ""
+        open = true
 
-		return TeXDocument(jobname, documentclass, documentfontsizept, packages, preamble, commands, inputs, build_dir, title, open)
-	end
+        return TeXDocument(jobname, documentclass, documentfontsizept, packages, preamble, commands, inputs, build_dir, title, open)
+    end
 
-	function TeXDocument(jobname::String)
-		tex = TeXDocument()
-		tex.jobname = jobname
-		return tex
-	end
+    function TeXDocument(jobname::String)
+        tex = TeXDocument()
+        tex.jobname = jobname
+        return tex
+    end
 end
 
 addpackage!(doc::TeXDocument, package::String) = push!(doc.packages, TeXPackage(package))
